dfa608ff46ffeb5c0514c5ee7d1ffbb1e4eacce4
Seyyed Ali Ayati <seyyedaliayati@gmail.com>
2021-08-06 14:06:45 +0430

    Replaced login and logout with built-in views

--- loog/main/urls.py
+++ loog/main/urls.py
@@ -1,4 +1,6 @@
+from django.contrib.auth import views as auth_views
 from django.urls import path
+
 from . import views
 
 app_name = "main"
@@ -7,7 +9,7 @@ app_name = "main"
 urlpatterns = [
     path("", views.HomePage.as_view(), name="homepage"),
     path("register/", views.RegisterPage.as_view(), name="register"),
-    path("login/", views.login_request, name="login"),
-    path("logout/", views.logout_request, name="logout"),
+    path("login/", auth_views.LoginView.as_view(), name="login"),
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
     path("search/", views.search, name="search"),
 ]
--- loog/main/views.py
+++ loog/main/views.py
@@ -1,54 +1,27 @@
-from django.shortcuts import render, redirect
-from django.views.generic.base import TemplateView
-from django.views.generic import CreateView
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.messages.views import SuccessMessageMixin
-from django.contrib.auth import login, authenticate, logout
-from django.contrib import messages
-from django.contrib.auth.forms import AuthenticationForm
 from django.urls import reverse_lazy
+from django.utils.translation import gettext_lazy as _
+from django.views import generic
 
 from discovery.views import discover
 
 
-
-class HomePage(TemplateView):
+class HomePage(generic.TemplateView):
+    """
+    Simple template view for rendering the home page of website.
+    """
     template_name = "main/main.html"
 
 
-class RegisterPage(SuccessMessageMixin, CreateView):
+class RegisterPage(SuccessMessageMixin, generic.CreateView):
+    """
+    Creates a new user based on built-in django user model.
+    """
     template_name = "main/register.html"
     form_class = UserCreationForm
     success_url = reverse_lazy('main:login')
-    success_message = "Your profile was created successfully"
-
-
-
-def login_request(request):
-    if request.method == "POST":
-        form = AuthenticationForm(request, data=request.POST)
-        if form.is_valid():
-            username = form.cleaned_data.get("username")
-            password = form.cleaned_data.get("password")
-            user = authenticate(username=username, password=password)
-            if user is not None:
-                login(request, user)
-                messages.info(request, f"You are now logged in as {username}.")
-                return redirect("main:homepage")
-            else:
-                messages.error(request, "Invalid username or password.")
-        else:
-            messages.error(request, "Invalid username or password.")
-    form = AuthenticationForm()
-    return render(
-        request=request, template_name="main/login.html", context={"login_form": form}
-    )
-
-
-def logout_request(request):
-    logout(request)
-    messages.info(request, "You have successfully logged out.")
-    return redirect("main:homepage")
+    success_message = _("Your profile was created successfully")
 
 
 def search(request):
