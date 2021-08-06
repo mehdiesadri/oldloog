289b2abfd13468ac9fbbf8c53db95f5b757a9e4c
Seyyed Ali Ayati <seyyedaliayati@gmail.com>
2021-08-07 02:45:02 +0430

    Added Docker

--- loog/loog/settings/production.py
+++ loog/loog/settings/production.py
@@ -8,8 +8,12 @@ DEBUG = False
 # TODO: Use MySQL Config for production!
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": os.environ.get("DATABASE_NAME"),
+        "USER": os.environ.get("DATABASE_USER"),
+        "PASSWORD": os.environ.get("DATABASE_PASSWORD"),
+        "HOST": os.environ.get("DATABASE_HOST"),
+        "PORT": os.environ.get("DATABASE_PORT")
     }
 }
 
