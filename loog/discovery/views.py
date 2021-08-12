afec86a7a2bbdeea9100b99d6a6435316f101b0f
Seyyed Ali Ayati <ayati_a@comp.iust.ac.ir>
2021-08-12 10:51:22 +0430

    Added query parser, simple inverted index, and ngrams

--- loog/discovery/views.py
+++ loog/discovery/views.py
@@ -1,8 +1,17 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import HttpResponse
 from django.views import generic
 
 from core.mixins import ProfileRequiredMixin
+from .utils import find_users
 
 
 class IndexPage(ProfileRequiredMixin, LoginRequiredMixin, generic.TemplateView):
     template_name = 'discovery/index.html'
+
+
+def search(request):
+    query = request.GET.get("query", "")
+    user_score = find_users(query)
+    print(user_score)
+    return HttpResponse("OK")
