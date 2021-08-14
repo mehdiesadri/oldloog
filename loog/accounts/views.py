275df77f665900d418a5ce0f262fd5a23444efe3
Seyyed Ali Ayati <ayati_a@comp.iust.ac.ir>
2021-08-14 15:02:44 +0430

    Fixed register field errors

--- loog/accounts/views.py
+++ loog/accounts/views.py
@@ -51,8 +51,6 @@ class RegisterView(generic.View):
 
     def post(self, request, uidb64_invite_id, token):
         invite_obj = get_invite_obj_from_url(uidb64_invite_id)
-        print(invite_obj)
-        print(registration_token.check_token(invite_obj.inviter, token))
         if invite_obj is not None and registration_token.check_token(invite_obj.inviter, token):
             form = RegisterForm(request.POST)
             if form.is_valid():
@@ -74,10 +72,10 @@ class RegisterView(generic.View):
                             receiver=invite_obj.inviter,
                             giver=new_user
                         )
+
                 return redirect("accounts:login")
             else:
-                print(form.errors)
-                # TODO: Fix form view
+                return render(request, 'accounts/register.html', context={'form': form, 'inviter': invite_obj.inviter})
         return HttpResponseForbidden(_("Sorry! You don't have access to this link..."))
 
 
