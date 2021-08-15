d8a7a5062c633a39e819977292c3ae579dcb1e88
Seyyed Ali Ayati <ayati_a@comp.iust.ac.ir>
2021-08-15 10:32:52 +0430

    Added scroll to tag list in profile.html

--- loog/discovery/utils.py
+++ loog/discovery/utils.py
@@ -12,7 +12,7 @@ INVERTED_INDEX = {}
 
 
 def get_tag_counts_in_assignments(assignments) -> dict:
-    annotated_tags = assignments.values_list('tag__name').annotate(total=Count('tag')).order_by('-total')
+    annotated_tags = assignments.values_list('tag__name').annotate(total=Count('tag')).order_by('-total')[:100]
     tag_counts = {}
     for tag_name, tag_count in annotated_tags:
         tag_counts[tag_name] = tag_count
