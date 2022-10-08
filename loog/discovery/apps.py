794627fdebb5fd693ef856bdd2ab89158edb0d41
Ali Ayati <seyyedaliayati@gmail.com>
2022-10-09 00:06:53 +0330

    Added TODOs for discovery app

--- loog/discovery/apps.py
+++ loog/discovery/apps.py
@@ -1,6 +1,5 @@
 from django.apps import AppConfig
 
 
-class DiscoveryConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
+class DiscoveryConfig(AppConfig):    
     name = "discovery"
